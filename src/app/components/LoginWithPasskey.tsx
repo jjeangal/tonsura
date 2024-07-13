9dcc9452986740d32d1b106298b1d8334e70b8fe
JEAN GAL <jgatbkk@gmail.com>
2024-07-13 15:05:03 +0200

    update style login with pass key

--- src/app/components/LoginWithPasskey.tsx
+++ src/app/components/LoginWithPasskey.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Box, VStack, Heading, Text, StackDivider } from '@chakra-ui/react'
+import { Button, Divider, Box, VStack, Heading, Text, StackDivider, Center } from '@chakra-ui/react'
 import { PasskeyArgType } from '@safe-global/protocol-kit'
 import { useState } from 'react'
 import { loadPasskeysFromLocalStorage } from '../lib/passkeys'
@@ -13,54 +13,57 @@ function LoginWithPasskey({ handleCreatePasskey, handleSelectPasskey }: Props) {
     const [passkeys, setPasskeys] = useState<PasskeyArgType[]>([])
 
     return (
-        <Box
-            margin="32px auto 0"
-            p={8}
-            boxShadow="md"
-            borderRadius="md"
-            width="100%"
-            maxWidth="600px"
-            bg="background.paper"
-        >
-            <VStack spacing={8} divider={<StackDivider borderColor="border.light" />}>
-                <Heading as="h1" size="lg" textAlign="center" color="primary.main">
-                    Use Safe Account via Passkeys
-                </Heading>
+        <Center height="100vh">
+            <Box
+                p={8}
+                boxShadow="md"
+                borderRadius="md"
+                width="100%"
+                maxWidth="600px"
+                bg="background.paper"
+                border="1px solid"
+                borderColor="border.light"
+            >
+                <VStack spacing={8} divider={<StackDivider borderColor="border.light" />}>
+                    <Heading as="h1" size="lg" textAlign="center" color="primary.main">
+                        Use Safe Account via Passkeys
+                    </Heading>
 
-                <Text textAlign="center" fontSize="xl">
-                    Create a new Safe using Passkeys
-                </Text>
+                    <Text textAlign="center" fontSize="xl">
+                        Create a new Safe using Passkeys
+                    </Text>
 
-                <Button
-                    onClick={handleCreatePasskey}
-                    leftIcon={<BellIcon />}
-                    variant="outline"
-                    mb={6}
-                >
-                    Create a new Passkey
-                </Button>
+                    <Button
+                        onClick={handleCreatePasskey}
+                        leftIcon={<BellIcon />}
+                        variant="outline"
+                        mb={6}
+                    >
+                        Create a new Passkey
+                    </Button>
 
-                <Divider borderColor="border.light" />
+                    <Divider borderColor="border.light" />
 
-                <Text textAlign="center" fontSize="xl">
-                    Connect existing Safe using an existing PassKey
-                </Text>
+                    <Text textAlign="center" fontSize="xl">
+                        Connect existing Safe using an existing PassKey
+                    </Text>
 
-                <Button
-                    leftIcon={<BellIcon />}
-                    variant="solid"
-                    colorScheme="blue"
-                    onClick={async () => {
-                        const passkeys = loadPasskeysFromLocalStorage()
+                    <Button
+                        leftIcon={<BellIcon />}
+                        variant="solid"
+                        colorScheme="blue"
+                        onClick={async () => {
+                            const passkeys = loadPasskeysFromLocalStorage()
 
-                        setPasskeys(passkeys)
-                        handleSelectPasskey(passkeys[0])
-                    }}
-                >
-                    Use an existing Passkey
-                </Button>
-            </VStack>
-        </Box>
+                            setPasskeys(passkeys)
+                            handleSelectPasskey(passkeys[0])
+                        }}
+                    >
+                        Use an existing Passkey
+                    </Button>
+                </VStack>
+            </Box>
+        </Center>
     )
 }
 
