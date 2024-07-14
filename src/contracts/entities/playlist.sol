5835d345cfa742bad6021d2a86fb00fe3ad2711f
JEAN GAL <jgatbkk@gmail.com>
2024-07-14 05:59:10 +0200

    change song and playlist contract

---
+++ src/contracts/entities/playlist.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ENS} from "@ensdomains/ens-contracts/contracts/registry/ENS.sol";
+import {ReverseClaimer} from "@ensdomains/ens-contracts/contracts/reverseRegistrar/ReverseClaimer.sol";
+
+import "./song.sol";
+
+contract Playlist is ReverseClaimer {
+    Song[] public songs;
+    string public metadata;
+
+    event PlaylistCreated(
+        uint256 playlistId,
+        string metadata,
+        address songAddress
+    );
+
+    constructor(
+        ENS ens,
+        string memory _metadata
+    ) ReverseClaimer(ens, msg.sender) {
+        metadata = _metadata;
+    }
+
+    function setMetadata(string memory _metadata) public {
+        metadata = _metadata;
+    }
+}
